31cb7c8a38a2fd482ad411385655d5731aa4496e
Eliana <enicotra83@gmail.com>
2022-07-06 23:05:39 -0300

    descarga stock en el backend

--- frontend/main.js
+++ frontend/main.js
@@ -1,50 +1,59 @@
-let productos = []
+let carrito = []
 let total = 0
-let carrito = document.getElementById('carrito')
+let divCarrito = document.getElementById('carrito')
 let precioTotal = document.getElementById('precioTotal')
 let containerProductos = document.getElementById('containerProductos')
 
-let agregar = (producto, precio)=>{
-    console.log(producto, precio)
-    productos.push(producto)
-    total = total + precio
-    precioTotal.innerHTML=`$ ${total}.-`
-    }
-
-let verCarrito = () =>{
-    alert(productos.join(", \n"))
-    console.log(productos)
-    console.log(total)
+let agregar = (productoId, precio) => {
+  console.log(productoId, precio)
+  carrito.push(productoId)
+  total = total + precio
+  precioTotal.innerHTML = `$ ${total}.-`
 }
 
-carrito.addEventListener('click', () =>{
-    verCarrito()
+async function verCarrito() {
+  const productList = await (
+    await fetch('/api/pagar', {
+      method: 'post',
+      body: JSON.stringify(carrito),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  ).json()
+
+  //alert(productosCarrito.join(", \n"))
+  //console.log(productos)
+  //console.log(total)
+}
+
+divCarrito.addEventListener('click', () => {
+  verCarrito()
 })
 
-  //cuando arranque la app en el index, que me haga un fetch de los productos - despues de iniciar el back
-  window.onload = async() => {
-    //console.log('fetch')
-    const productList = await (await fetch("/api/productos")).json()
-    console.log(productList)
-    displayProductos(productList)
-  }
+//cuando arranque la app en el index, que me haga un fetch de los productos - despues de iniciar el back
+window.onload = async () => {
+  //console.log('fetch')
+  const productList = await (await fetch('/api/productos')).json()
+  console.log(productList)
+  displayProductos(productList)
+}
 
-  const displayProductos= (productList)=>{
-    let productHTML = ''
-    productList.forEach(element => {
-        productHTML+=
-        `<div class="col">
+const displayProductos = (productList) => {
+  let productHTML = ''
+  productList.forEach((element) => {
+    productHTML += `<div class="col">
         <div class="productCard card h-100">
             <h3>${element.name}</h3>
             <img src="${element.image}"
-                alt="${element.image}" class="prodImg">
+                alt="${element.name}" class="prodImg">
             <p>${element.description}</p>
             <h4><strong>$ ${element.price}.-</strong></h4>
-            <button class="btn btn-warning" onClick="agregar('${element.name}', ${element.price})"><span class="material-icons">
+            <button class="btn btn-warning" onClick="agregar('${element.id}', ${element.price})"><span class="material-icons">
                     add_shopping_cart
                 </span></button>
         </div>
     </div>`
-    });
-    containerProductos.innerHTML = productHTML
-  }
+  })
+  containerProductos.innerHTML = productHTML
+}
--- index.js
+++ index.js
@@ -10,62 +10,79 @@ const port = 3000
 //   res.send('Hello World!')
 // })
 
-// cuando corro la app, me da la url para porder verla
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// para leer el post de pagar como json
+app.use(express.json())
 
 // backend de la pagina
 const productos = [
   {
-    id: 001,
+    id: 1,
     name: 'Wapsipinicon Peach',
     description: 'Tomate mediano amarillo de textura aterciopelada',
     price: 500,
     image: 'media/producto1',
-    stock: 50,
+    stock: 4,
   },
   {
-    id: 002,
+    id: 2,
     name: 'Black Keyes',
     description: 'Tomate mediano amarillo de textura aterciopelada',
     price: 500,
     image: 'media/producto1',
-    stock: 50,
-  },  {
-    id: 003,
+    stock: 2,
+  },
+  {
+    id: 3,
     name: 'Aperitivo Dulce',
     description: 'Tomate mediano amarillo de textura aterciopelada',
     price: 500,
     image: 'media/producto1',
-    stock: 50,
-  },  {
-    id: 004,
+    stock: 3,
+  },
+  {
+    id: 4,
     name: 'Bonsai',
     description: 'Tomate mediano amarillo de textura aterciopelada',
     price: 500,
     image: 'media/producto1',
-    stock: 50,
-  },  {
-    id: 005,
+    stock: 5,
+  },
+  {
+    id: 5,
     name: 'Ildi',
     description: 'Tomate mediano amarillo de textura aterciopelada',
     price: 500,
     image: 'media/producto1',
-    stock: 50,
-  },  {
-    id: 006,
+    stock: 2,
+  },
+  {
+    id: 6,
     name: 'Amarillo',
     description: 'Tomate mediano amarillo de textura aterciopelada',
     price: 500,
     image: 'media/producto1',
-    stock: 50,
+    stock: 1,
   },
 ]
 //cuando la app hace un get de /api/productos, me retorna los productos
 app.get('/api/productos', (req, res) => {
-    res.send(productos)
+  res.send(productos)
+})
+//traigo con post el detalle de productos agregados al carrito
+// npm install body-parser para poder leer el body del req como json
+app.post('/api/pagar', (req, res) => {
+  //   console.log(req.body)
+  const ids = req.body
+  ids.forEach((id) => {
+    const product = productos.find((p) => p.id == id)
+    product.stock--
   })
+  res.send(productos)
+})
 // cuando la app va a /, me lleva al index.html de la carpeta frontend
-  app.use('/', express.static('frontend'))
+app.use('/', express.static('frontend'))
 
+// cuando corro la app, me da la url para porder verla
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
