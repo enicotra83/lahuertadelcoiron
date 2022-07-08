d6137d0f62fc9d4e67d17957f64276ddae5313e5
Eliana <enicotra83@gmail.com>
2022-07-08 11:21:13 -0300

    descarga stock en el frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,55 @@
+let productList = []
 let carrito = []
 let total = 0
 let divCarrito = document.getElementById('carrito')
 let precioTotal = document.getElementById('precioTotal')
 let containerProductos = document.getElementById('containerProductos')
 
+divCarrito.addEventListener('click', () => {
+  verCarrito()
+})
+
+//cuando arranque la app en el index, que me haga un fetch de los productos - despues de iniciar el back
+window.onload = async () => {
+  //console.log('fetch')
+  productList = await (await fetch('/api/productos')).json()
+  console.log(productList)
+  displayProductos()
+}
+
 let agregar = (productoId, precio) => {
+  const product = productList.find((p) => p.id == productoId)
+  product.stock--
   console.log(productoId, precio)
   carrito.push(productoId)
   total = total + precio
   precioTotal.innerHTML = `$ ${total}.-`
+  displayProductos()
 }
 
+const displayProductos = () => {
+  let productHTML = ''
+  productList.forEach((p) => {
+    let buttonHTML = `<button class="btn btn-warning" onClick="agregar('${p.id}', ${p.price})"><span class="material-icons">
+ add_shopping_cart</span></button>`
+    if (p.stock <= 0) {
+      buttonHTML = `<button  disabled class="btn btn-warning" onClick="agregar('${p.id}', ${p.price})"><span class="material-icons">compost</span></button>`
+     
+    }
+    productHTML += `<div class="col">
+        <div class="productCard card h-100">
+            <h3>${p.name}</h3>
+            <img src="${p.image}"
+                alt="${p.name}" class="prodImg">
+            <p>${p.description}</p>
+            <p>Stock: ${p.stock} unidades</p>
+            <h4><strong>$ ${p.price}.-</strong></h4>
+            ${buttonHTML}
+            </div>
+        </div>`
+  })
+  containerProductos.innerHTML = productHTML
+}
 async function verCarrito() {
   const productList = await (
     await fetch('/api/pagar', {
@@ -25,35 +64,4 @@ async function verCarrito() {
   //alert(productosCarrito.join(", \n"))
   //console.log(productos)
   //console.log(total)
-}
-
-divCarrito.addEventListener('click', () => {
-  verCarrito()
-})
-
-//cuando arranque la app en el index, que me haga un fetch de los productos - despues de iniciar el back
-window.onload = async () => {
-  //console.log('fetch')
-  const productList = await (await fetch('/api/productos')).json()
-  console.log(productList)
-  displayProductos(productList)
-}
-
-const displayProductos = (productList) => {
-  let productHTML = ''
-  productList.forEach((element) => {
-    productHTML += `<div class="col">
-        <div class="productCard card h-100">
-            <h3>${element.name}</h3>
-            <img src="${element.image}"
-                alt="${element.name}" class="prodImg">
-            <p>${element.description}</p>
-            <h4><strong>$ ${element.price}.-</strong></h4>
-            <button class="btn btn-warning" onClick="agregar('${element.id}', ${element.price})"><span class="material-icons">
-                    add_shopping_cart
-                </span></button>
-        </div>
-    </div>`
-  })
-  containerProductos.innerHTML = productHTML
 }
